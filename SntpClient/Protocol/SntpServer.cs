c74d500c93ad182a981ad8cfec39c853438d25cf
Mauricio Parra Casado <mauryparra@gmail.com>
2013-08-09 12:48:29 -0300

    Class Library added to project

---
+++ SntpClient/Protocol/SntpServer.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace SntpClient.Protocol
+{
+    /// <summary>
+    /// A class that holds information to connect to a remote NTP/SNTP Server
+    /// </summary>
+    class SntpServer
+    {
+    }
+}
